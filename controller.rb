61578601b8a8570cbd73de76c662ea8f0f43054b
Gillian Shanks <GillianShanks88@gmail.com>
2019-08-05 16:47:38 +0100

    adds winner.erb to print a <h1> calling on result. Modified controller.rb to use params[:hand1] etc.

--- controller.rb
+++ controller.rb
@@ -6,6 +6,7 @@ also_reload('./models/*.rb')
 
 get '/:hand1/:hand2' do
   game = Game.new()
-  @result = game.play(:hand1,:hand2)
+  @result = game.play(params[:hand1],params[:hand2])
+
   erb(:winner)
 end
