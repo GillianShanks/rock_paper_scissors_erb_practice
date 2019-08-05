75a4348000ebda13228c9b9405e5318229dd3c3d
Gillian Shanks <GillianShanks88@gmail.com>
2019-08-05 17:47:46 +0100

    adds layout.erb and links to welcome page from winner.erb.

--- controller.rb
+++ controller.rb
@@ -5,8 +5,11 @@ require_relative('./models/game.rb')
 also_reload('./models/*.rb')
 
 get '/:hand1/:hand2' do
-  @result = Game.play(params[:hand1].downcase(),params[:hand2].downcase())
+  # result = Game.play(params[:hand1].downcase(),params[:hand2].downcase())
+  # return "#{result} wins!"
 
+  @result = Game.play(params[:hand1].downcase(),params[:hand2].downcase())
+  
   erb(:winner)
 end
 
